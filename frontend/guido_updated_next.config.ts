0c47bfd8f225b37f5b7cb892b01c171cec679ece
Guido Diepen <site-github@guidodiepen.nl>
2025-02-06 08:28:25 +0100

    Include the env stuff

--- frontend/guido_updated_next.config.ts
+++ frontend/guido_updated_next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	output: "standalone",
-  env: {},
+        output: "standalone",
+    env: {
+    BEN_ERBIJ_BASE_URL: process.env.BEN_ERBIJ_BASE_URL,
+  },
 };
 
 export default nextConfig;
+
