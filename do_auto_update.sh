b2c4a535eb568cec699bece3b4b899a5efb7d4be
Guido Diepen <site-github@guidodiepen.nl>
2025-02-06 08:31:58 +0100

    Add auto update script

---
+++ do_auto_update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+while true ; do
+
+	./do_build.sh
+
+        echo
+        echo
+        echo
+        echo
+        echo
+        echo
+        echo "Sleeping for 1 minute to check for new version"
+        sleep 60
+done
+
